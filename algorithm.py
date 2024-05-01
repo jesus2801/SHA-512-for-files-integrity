a3b70b2082bbb743ae1c4ed63c081f779f848459
jesus2801 <jesusd280147@gmail.com>
2024-05-01 13:21:18 -0500

    program works

---
+++ algorithm.py
@@ -0,0 +1,18 @@
+import hashlib
+
+BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+
+def SHA512(file_path: str):
+  sha512 = hashlib.sha512()
+
+  with open(file_path, 'rb') as f:
+    while True:
+      data = f.read(BUF_SIZE)
+      if not data:
+        break
+      sha512.update(data)
+
+  return sha512.digest()
+
+# print("SHA1: {0}".format(sha1.hexdigest()))
